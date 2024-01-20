9f226e4389626bdfa67e17524d3b02f3d55d416f
Harsh Rajput <harshmori0521@gmail.com>
2024-01-20 14:18:53 +0530

    event model added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:csi_app/providers/bottom_navigation_provider.dart';
 import 'package:csi_app/screens/on_boading_screens/splash_screen.dart';
+import 'package:csi_app/utils/colors.dart';
 import 'package:flutter/material.dart' ;
 import 'package:flutter_hidden_drawer/flutter_hidden_drawer.dart';
 
