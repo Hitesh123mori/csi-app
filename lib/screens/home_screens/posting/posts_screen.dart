4ac82d29374a9da75443ab124cd4c9d79ed8e3ec
Harsh Rajput <harshmori0521@gmail.com>
2024-03-08 20:36:57 +0530

    harsh final

--- lib/screens/home_screens/posting/posts_screen.dart
+++ lib/screens/home_screens/posting/posts_screen.dart
@@ -1,12 +1,15 @@
 import 'package:csi_app/apis/FirebaseAPIs.dart';
 // import 'package:csi_app/apis/FirebaseDatabaseAPIs/PostAPI.dart';
 import 'package:csi_app/models/post_model/post.dart';
+import 'package:csi_app/side_transition_effects/bottom_top.dart';
 import 'package:flutter/material.dart';
 
 // import 'package:csi_app/constants/dummy_post.dart';
 import 'package:csi_app/main.dart';
 import 'package:csi_app/utils/colors.dart';
 import 'package:csi_app/utils/widgets/posting/post_card.dart';
+
+import 'add_post_screen.dart';
 
 class PostsScreen extends StatefulWidget {
   const PostsScreen({super.key});
@@ -24,6 +27,7 @@ class _PostsScreenState extends State<PostsScreen> {
         backgroundColor: AppColors.theme['backgroundColor'],
         floatingActionButton: FloatingActionButton(
           onPressed: (){
+            Navigator.push(context, BottomToTop(AddPostScreen()));
           },
           child: Icon(Icons.add,color: AppColors.theme['secondaryColor'],),
           backgroundColor: AppColors.theme['primaryColor'],
